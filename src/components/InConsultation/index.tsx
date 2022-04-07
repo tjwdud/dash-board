e384065c06a521047c784534af3db38826e39e7b
sjy <stseo012@gmail.com>
2022-04-07 11:26:20 +0900

    [markup]: 상담중 컴포넌트

---
+++ src/components/InConsultation/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Text } from "./style";
+
+function InConsultation() {
+  return (
+    <Container>
+      <Text>상담중</Text>
+    </Container>
+  );
+}
+
+export default InConsultation;
