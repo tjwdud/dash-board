9f5db2952e95bd2f1cbdf6fc8ae74c44c3a5b08a
sjy <stseo012@gmail.com>
2022-04-08 21:57:18 +0900

    [feat]: 모바일 슬라이드 메뉴 추가

---
+++ src/components/Header/MobileMenu/index.tsx
@@ -0,0 +1,48 @@
+import React, { forwardRef, useEffect, useRef } from "react";
+import { Divider } from "components/Card/style";
+import { Text14 } from "styles/text";
+import {
+  MenuContainer,
+  MenuBody,
+  Logo,
+  MenuHeader,
+  MenuListWrapper,
+  MenuList,
+  CompanyIcon,
+} from "./style";
+
+interface Props {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+function MobileMenu({ open, setOpen }: Props) {
+  const handleClickOutside = () => {
+    setOpen(false);
+  };
+  return (
+    <MenuContainer onClick={() => handleClickOutside()}>
+      <MenuBody
+        open={open}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <MenuHeader>
+          <Logo />
+          <Divider />
+        </MenuHeader>
+        <MenuListWrapper>
+          <MenuList>
+            <CompanyIcon />
+            <Text14>파트너정밀가공</Text14>
+          </MenuList>
+          <MenuList>
+            <Text14>로그아웃</Text14>
+          </MenuList>
+        </MenuListWrapper>
+      </MenuBody>
+    </MenuContainer>
+  );
+}
+
+export default MobileMenu;
