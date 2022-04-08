d8000001099c3d556845f787fc6604d2249da14e
sjy <stseo012@gmail.com>
2022-04-08 21:58:06 +0900

    [feat]: 반응형 헤더 & 슬라이드 메뉴 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { MainText } from "styles/text";
 import {
   HeaderContainer,
@@ -7,11 +7,20 @@ import {
   CompanyName,
   Profile,
   Divider,
+  MenuIcon,
 } from "./style";
+import MobileMenu from "./MobileMenu/index";
 
 function Header() {
+  const [open, setOpen] = useState(false);
+  const menuHandler = () => {
+    setOpen((prev) => !prev);
+  };
+
   return (
     <HeaderContainer>
+      <MenuIcon onClick={() => menuHandler()} />
+      {open && <MobileMenu setOpen={setOpen} open={open} />}
       <HeaderImg />
       <Profile>
         <CompanyIcon />
