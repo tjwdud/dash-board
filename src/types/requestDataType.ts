b8e2590754f809c131cc9b93ea4792c8082d8300
sjy <stseo012@gmail.com>
2022-04-08 17:06:11 +0900

    [feat]: db data type 추가

---
+++ src/types/requestDataType.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface IRequests {
+  id: number;
+  title: string;
+  client: string;
+  due: string;
+  count: number;
+  amount: number;
+  method: string[];
+  material: string[];
+  status: string;
+}
